ef084c1721f40e241f655641482e246e0fc3feb9
Pemberai Sweto <pembeweb@yahoo.com>
2020-04-06 22:46:32 +0200

    feat: update port variable config

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,6 @@
 const app = require('./server.js')
 
-const port = process.env.PORT || 3000
-
-app.listen(port, () => {
-  console.log(`Server listening on port: ${port}`)
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`)
 })
